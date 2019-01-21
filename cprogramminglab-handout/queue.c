99b1504ce3412929ef11f1519567b646466d3174
Matthew Henderson <mlhender@andrew.cmu.edu>
2019-01-20 19:54:46 -0500

    test

--- cprogramminglab-handout/queue.c
+++ cprogramminglab-handout/queue.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * Code for basic C skills diagnostic.
  * Developed for courses 15-213/18-213/15-513 by R. E. Bryant, 2017
  * Modified to store strings, 2018
@@ -24,6 +24,7 @@
 */
 queue_t *q_new()
 {
+    testtestest
     queue_t *q =  malloc(sizeof(queue_t));
     /* What if malloc returned NULL? */
     q->head = NULL;
